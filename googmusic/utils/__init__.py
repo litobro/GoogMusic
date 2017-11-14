1e5954e86af5d28406ce392c82cf44c676205d09
Thomas Dang <thomas.kyle.dang@gmail.com>
2017-11-14 16:49:47 -0700

    added MusicQueue framework for advanced playback functionality

--- googmusic/utils/__init__.py
+++ googmusic/utils/__init__.py
@@ -1,1 +1,2 @@
 from . import musicmanager
+from . import queue
--- googmusic/utils/queue.py
+++ googmusic/utils/queue.py
@@ -1,3 +1,39 @@
 from builtins import object
 
+class MusicQueue(object):
+    _queue = []
+    _index = None
+    _loop = False
 
+    def __init__(self):
+        pass
+
+    def enqueue(self, track):
+        if len(self._queue) == 0:
+            self._index = 0
+        self._queue.append(track)
+
+    def next(self):
+        if len(self._queue) <= _index + 1:
+            self._index += 1
+            return self._queue[self._index]
+        elif self._loop:
+            self._index = 0
+            return self._queue[self._index]
+        return None
+
+    def prev(self):
+        if self._index <= 0 and self._loop:
+            self._index = len(self._queue)
+        if self._index is not None:
+            self._index -= 1
+            return self._queue[self._index]
+        else:
+            return None
+
+    def current(self):
+        return self._queue[self._index]
+
+    def clear(self):
+        self._index = None
+        self._queue = []
